8e2e40bd67b7df7f64ab1916c4528285e81192e5
Gordon B. Isnor <gordonbisnor@gmail.com>
2016-12-12 22:31:28 -0400

    Update to 1.10

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:mupdf][:version] = '1.6'
+default[:mupdf][:version] = '1.10'
 
 if node[:kernel][:machine] == 'x86_64'
 	default[:mupdf][:arch] = 'amd64'
@@ -6,4 +6,4 @@ else
 	default[:mupdf][:arch] = 'i386'
 end
 
-default[:mupdf][:checksum] = '491d7a3b131589791c7df6dd8161c6bfe41ce74a'
+default[:mupdf][:checksum] = 'aacc1f36b9180f562022ef1ab3439b009369d944364f3cff8a2a898834e3a836'
